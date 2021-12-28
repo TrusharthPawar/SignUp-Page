4ba29e03f1ceaaae0a49ec8723a2771dfc7fa9cc
TrusharthPawar <75682810+TrusharthPawar@users.noreply.github.com>
2021-12-28 15:27:33 +0530

    Bug fix

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   input.value === "" ? window.alert("Enter username") : console.log("ok");
   password.value === "" ? window.alert("Enter Password") : console.log("ok");
-  password.textLength >= 8
-    ? console.log("ok")
-    : window.alert("password contains atleast 8 charaters");
+  if (password.textLength === 0) {
+    console.log("enter details");
+  } else if (password.textLength > 0) {
+    password.textLength < 8
+      ? window.alert("password contains atleast 8 charaters")
+      : window.alert("Logged in Successfully");
+  }
 });
